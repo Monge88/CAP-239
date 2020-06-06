d7cdf45ba31cca573e77af28a03a41cb885ea264
André Oliveira <andreh_oliveira@hotmail.com>
2020-06-06 01:31:08 -0300

    Update cullen_frey.py

--- cullen_frey.py
+++ cullen_frey.py
@@ -83,7 +83,7 @@ def graph(df,method='unbiased',discrete=False,boot=None):
         s2_lnorm = (es2_lnorm+2)**2*(es2_lnorm-1)
         y_lnorm = kurtmax-(es2_lnorm**4+2*es2_lnorm**3+3*es2_lnorm**2-3)
 
-        plt.figure(figsize=(14,8))
+        plt.figure(figsize=(12,6))
 
         #observations
         obs = plt.scatter(skewdata**2,kurtmax-kurtdata,s=200, c='blue', 
@@ -153,7 +153,7 @@ def graph(df,method='unbiased',discrete=False,boot=None):
         s2_poisson = 1/lambda_
         y_poisson = kurtmax-(3+1/lambda_)
     
-        plt.figure(figsize=(12,9))          
+        plt.figure(figsize=(12,6))          
     
         #observations
         obs = plt.scatter(skewdata**2,kurtmax-kurtdata,s=200, c='blue', 
